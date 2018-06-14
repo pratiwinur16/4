92f8306f3106e897d4a54d2ee8e5caa103b927bb
pratiwi <pratiwinur.amini@gmail.com>
2018-06-14 12:08:35 +0700

    tukar ukuran

---
+++ Exercise-9.js
@@ -0,0 +1,24 @@
+/*Diberikan function tukarBesarKecil(kalimat) yang menerima satu parameter berupa string. Function akan me-return string tersebut dengan menukar karakter yang uppercase menjadi lowercase, dan sebaliknya. 
+Spasi dan simbol diabaikan.*/
+
+
+function tukarBesarKecil(kalimat) {
+  var result = ""
+  for (var i = 0; i < kalimat.length; i++){
+    if (kalimat[i] === kalimat[i].toUpperCase()){
+      result = result + (kalimat[i].toLowerCase())
+    } else if  (kalimat [i] === kalimat[i].toLowerCase()) {
+      result = result + (kalimat[i].toUpperCase())
+    } else {
+      result = result + kalimat[i]
+    }
+  }
+return result 
+}
+
+// TEST CASES
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
